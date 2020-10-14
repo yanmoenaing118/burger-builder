3345cfb506c5dd60e9ad2ed92717d3f3a175f31a
yanmoenaing118 <yan.webdev.1234@gmail.com>
2020-10-14 10:01:24 +0630

    Burger Builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Aux from "./../../hoc/Aux";
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Burger Conotrols</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
